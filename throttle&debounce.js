37bd800b832d99a08a5e1c201af7a57b14f44f1b
iamamanpandey <iamamanpandey007@gmail.com>
2023-11-08 22:00:47 +0530

    added throttle

---
+++ throttle&debounce.js
@@ -0,0 +1,14 @@
+//throttle 
+function debounce(func, delay) {
+    let timerId;
+    return function () {
+        const context = this;
+        const args = arguments;
+        clearTimeout(timerId);
+        timerId = setTimeout(function () {
+            func.apply(context, args);
+        }, delay);
+    };
+}
+
+// 
