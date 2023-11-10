75e9c048697767a034869914691ae9d71e5f8852
iamamanpandey <iamamanpandey007@gmail.com>
2023-11-10 21:38:13 +0530

    added promises

---
+++ promise.js
@@ -0,0 +1,28 @@
+let promise = new Promise(function(resolve, reject) {
+    resolve("done");
+  
+    reject(new Error("…")); // ignored
+    setTimeout(() => resolve("…")); // ignored
+  });
+
+  let promise2 = new Promise(function(resolve, reject) {
+    setTimeout(() => resolve("done!"), 1000);
+  });
+  
+  // resolve runs the first function in .then
+  promise2.then(
+    result => alert(result), // shows "done!" after 1 second
+    error => alert(error) // doesn't run
+  );
+
+  function loadScript(src) {
+    return new Promise(function(resolve, reject) {
+      let script = document.createElement('script');
+      script.src = src;
+  
+      script.onload = () => resolve(script);
+      script.onerror = () => reject(new Error(`Script load error for ${src}`));
+  
+      document.head.append(script);
+    });
+  }
