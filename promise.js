0e57ae4da93c5056933c45d3669407a5d55f29f2
iamamanpandey <iamamanpandey007@gmail.com>
2023-11-11 20:13:48 +0530

    added new example

--- promise.js
+++ promise.js
@@ -64,3 +64,23 @@ new Promise(function(resolve, reject) {
     return result * 2;
   
   });
+
+
+  let promise = new Promise(function(resolve, reject) {
+    setTimeout(() => resolve(1), 1000);
+  });
+  
+  promise.then(function(result) {
+    alert(result); // 1
+    return result * 2;
+  });
+  
+  promise.then(function(result) {
+    alert(result); // 1
+    return result * 2;
+  });
+  
+  promise.then(function(result) {
+    alert(result); // 1
+    return result * 2;
+  });
