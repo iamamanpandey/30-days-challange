d3f59cf5d4aa700ff188c7e01dbcf350c2fc5e53
iamamanpandey <iamamanpandey007@gmail.com>
2023-11-12 18:38:23 +0530

    added proxy

--- promise.js
+++ promise.js
@@ -100,4 +100,16 @@ new Promise(function(resolve, reject) {
     .then(responses => responses.forEach(
       response => alert(`${response.url}: ${response.status}`)
     ));
+
+
+    let target = {};
+let proxy = new Proxy(target, {}); // empty handler
+
+proxy.test = 5; // writing to proxy (1)
+alert(target.test); // 5, the property appeared in target!
+
+alert(proxy.test); // 5, we can read it from proxy too (2)
+
+for(let key in proxy) alert(key); // test, iteration works (3)
+
   
