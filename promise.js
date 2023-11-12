aa243ab43f4ba6627339830ec9e17e7e6c301c42
iamamanpandey <iamamanpandey007@gmail.com>
2023-11-12 18:37:03 +0530

    adde example for  promise all

--- promise.js
+++ promise.js
@@ -84,3 +84,20 @@ new Promise(function(resolve, reject) {
     alert(result); // 1
     return result * 2;
   });
+
+
+  let urls = [
+    'https://api.github.com/users/iliakan',
+    'https://api.github.com/users/remy',
+    'https://api.github.com/users/jeresig'
+  ];
+  
+  // map every url to the promise of the fetch
+  let requests = urls.map(url => fetch(url));
+  
+  // Promise.all waits until all jobs are resolved
+  Promise.all(requests)
+    .then(responses => responses.forEach(
+      response => alert(`${response.url}: ${response.status}`)
+    ));
+  
