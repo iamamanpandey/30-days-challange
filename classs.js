e6fd3eb737855105079e1937cb4683a275435fea
iamamanpandey <iamamanpandey007@gmail.com>
2023-11-17 20:55:24 +0530

    minor changes

--- classs.js
+++ classs.js
@@ -19,3 +19,19 @@ console.log(person); // Jane Doe
 
 person.setName('Jane Smith');
 console.log(person.getName()); // Jane Smith
+
+let meeting = {
+    attendees: [],
+    add(attendee) {
+        console.log(`${attendee} joined the meeting.`);
+        this.attendees.push(attendee);
+        return this;
+    },
+    get latest() {
+        let count = this.attendees.length;
+        return count == 0 ? undefined : this.attendees[count - 1];
+    }
+};
+
+meeting.add('John').add('Jane').add('Peter');
+console.log(`The latest attendee is ${meeting.latest}.`);
